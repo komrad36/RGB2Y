6a822b77e9b1d6ac7aa0bd6ebd069e401546a512
Kareem Omar <kareem.h.omar@gmail.com>
2018-09-29 22:05:27 -0700

    Performance improvements

--- RGB2Y.h
+++ RGB2Y.h
@@ -3,10 +3,10 @@
 *   RGB2Y
 *
 *	Author: Kareem Omar
-*	kareem.omar@uah.edu
+*	kareem.h.omar@gmail.com
 *	https://github.com/komrad36
 *
-*	Last updated Dec 9, 2016
+*	Last updated Sep 29, 2018
 *******************************************************************/
 //
 // Fastest CPU (AVX/SSE) implementation of RGB to grayscale.
@@ -17,15 +17,15 @@
 //
 // You can use equal weighting by calling the templated
 // function with weight set to 'false', or you
-// can specify custom weights in RGB2Y.h (only slightly slower).
+// can specify non-equal weights (only slightly slower).
 //
-// The default weights match OpenCV's default.
+// The default non-equal weights match OpenCV's default.
 //
 // For even more speed see the CUDA version:
 // github.com/komrad36/CUDARGB2Y
 //
-// If you do not have AVX2, uncomment the #define below to route the code
-// through only SSE isntructions. NOTE THAT THIS IS ABOUT 50% SLOWER.
+// If you do not have AVX2, uncomment the #define NO_AVX_PLEASE below to
+// use only SSE isntructions. NOTE THAT THIS IS ABOUT 50% SLOWER.
 // A processor with full AVX2 support is highly recommended.
 //
 // All functionality is contained in RGB2Y.h.
@@ -41,115 +41,70 @@ constexpr double B_WEIGHT = 0.114;
 constexpr double G_WEIGHT = 0.587;
 constexpr double R_WEIGHT = 0.299;
 
-
-// Internal; do NOT modify
-constexpr uint16_t B_WT = static_cast<uint16_t>(64.0 * B_WEIGHT + 0.5);
-constexpr uint16_t G_WT = static_cast<uint16_t>(64.0 * G_WEIGHT + 0.5);
-constexpr uint16_t R_WT = static_cast<uint16_t>(64.0 * R_WEIGHT + 0.5);
-
 #include <algorithm>
 #include <cstdint>
 #include <future>
 #include <immintrin.h>
 
-// 241
-template<const bool last_row_and_col, bool weight>
+// Internal; do NOT modify
+constexpr uint16_t B_WT = static_cast<uint16_t>(32768.0 * B_WEIGHT + 0.5);
+constexpr uint16_t G_WT = static_cast<uint16_t>(32768.0 * G_WEIGHT + 0.5);
+constexpr uint16_t R_WT = static_cast<uint16_t>(32768.0 * R_WEIGHT + 0.5);
+
+static const __m256i weight_vec = _mm256_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT);
+
+template<bool weight>
+void RGB2Y_ref(const uint8_t* __restrict const data, const int32_t cols, const int32_t rows, const int32_t stride, uint8_t* const __restrict out) {
+	if (!weight) {
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				const auto idx = 3 * (i*stride + j);
+				out[i*stride + j] = (static_cast<uint32_t>(data[idx]) + static_cast<uint32_t>(data[idx + 1]) + static_cast<uint32_t>(data[idx + 2]) + 1) / 3;
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				const auto idx = 3 * (i*stride + j);
+				out[i*stride + j] = uint8_t(B_WEIGHT * data[idx] + 0.5f) + uint8_t(G_WEIGHT * data[idx + 1] + 0.5f) + uint8_t(R_WEIGHT * data[idx + 2] + 0.5f);
+			}
+		}
+	}
+}
+
+template<bool last_row_and_col, bool weight>
 void process(const uint8_t* __restrict const pt, const int32_t cols_minus_j, uint8_t* const __restrict out) {
 	__m128i h3;
 	if (weight) {
-#ifdef NO_AVX_PLEASE
-		__m128i p1aL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
-		__m128i p1aH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 8))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
-		__m128i p1bL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 18))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
-		__m128i p1bH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 26))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
-		__m128i p2aL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 1))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
-		__m128i p2aH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 9))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
-		__m128i p2bL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 19))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
-		__m128i p2bH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 27))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
-		__m128i p3aL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 2))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
-		__m128i p3aH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 10))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
-		__m128i p3bL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 20))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
-		__m128i p3bH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 28))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
-		__m128i sumaL = _mm_adds_epu16(p3aL, _mm_adds_epu16(p1aL, p2aL));
-		__m128i sumaH = _mm_adds_epu16(p3aH, _mm_adds_epu16(p1aH, p2aH));
-		__m128i sumbL = _mm_adds_epu16(p3bL, _mm_adds_epu16(p1bL, p2bL));
-		__m128i sumbH = _mm_adds_epu16(p3bH, _mm_adds_epu16(p1bH, p2bH));
-		__m128i sclaL = _mm_srli_epi16(sumaL, 6);
-		__m128i sclaH = _mm_srli_epi16(sumaH, 6);
-		__m128i sclbL = _mm_srli_epi16(sumbL, 6);
-		__m128i sclbH = _mm_srli_epi16(sumbH, 6);
-		__m128i shftaL = _mm_shuffle_epi8(sclaL, _mm_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
-		__m128i shftaH = _mm_shuffle_epi8(sclaH, _mm_setr_epi8(-1, -1, -1, 18, 24, 30, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
-		__m128i shftbL = _mm_shuffle_epi8(sclbL, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, 0, 6, 12, -1, -1, -1, -1, -1, -1, -1));
-		__m128i shftbH = _mm_shuffle_epi8(sclbH, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1));
-		__m128i accumL = _mm_or_si128(shftaL, shftbL);
-		__m128i accumH = _mm_or_si128(shftaH, shftbH);
-		h3 = _mm_or_si128(accumL, accumH);
-#else
-		__m256i p1a = _mm256_mullo_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt))), _mm256_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
-		__m256i p1b = _mm256_mullo_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 18))), _mm256_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
-		__m256i p2a = _mm256_mullo_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 1))), _mm256_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
-		__m256i p2b = _mm256_mullo_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 19))), _mm256_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
-		__m256i p3a = _mm256_mullo_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 2))), _mm256_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
-		__m256i p3b = _mm256_mullo_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 20))), _mm256_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
-		__m256i suma = _mm256_adds_epu16(p3a, _mm256_adds_epu16(p1a, p2a));
-		__m256i sumb = _mm256_adds_epu16(p3b, _mm256_adds_epu16(p1b, p2b));
-		__m256i scla = _mm256_srli_epi16(suma, 6);
-		__m256i sclb = _mm256_srli_epi16(sumb, 6);
-		__m256i shfta = _mm256_shuffle_epi8(scla, _mm256_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
-		__m256i shftb = _mm256_shuffle_epi8(sclb, _mm256_setr_epi8(-1, -1, -1, -1, -1, -1, 0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1));
-		__m256i accum = _mm256_or_si256(shfta, shftb);
-		h3 = _mm_or_si128(_mm256_castsi256_si128(accum), _mm256_extracti128_si256(accum, 1));
-#endif
+		__m256i in1 = _mm256_mulhrs_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(pt))), weight_vec);
+		__m256i in2 = _mm256_mulhrs_epi16(_mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(pt + 15))), weight_vec);
+		__m256i mul = _mm256_packus_epi16(in1, in2);
+		__m256i b1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(0, 3, 6, -1, -1, -1, 11, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 4, 7, -1, -1, 9, 12, -1, -1, -1, -1, -1, -1));
+		__m256i g1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(1, 4, 7, -1, -1, 9, 12, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, 5, -1, -1, -1, 10, 13, -1, -1, -1, -1, -1, -1));
+		__m256i r1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(2, 5, -1, -1, -1, 10, 13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 3, 6, -1, -1, 8, 11, 14, -1, -1, -1, -1, -1, -1));
+		__m256i accum = _mm256_adds_epu8(r1, _mm256_adds_epu8(b1, g1));
+		h3 = _mm_adds_epu8(_mm256_castsi256_si128(accum), _mm256_extracti128_si256(accum, 1));
 	}
 	else {
-#ifdef NO_AVX_PLEASE
-		__m128i p1aL = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt)));
-		__m128i p1aH = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 8)));
-		__m128i p1bL = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 18)));
-		__m128i p1bH = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 26)));
-		__m128i p2aL = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 1)));
-		__m128i p2aH = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 9)));
-		__m128i p2bL = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 19)));
-		__m128i p2bH = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 27)));
-		__m128i p3aL = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 2)));
-		__m128i p3aH = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 10)));
-		__m128i p3bL = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 20)));
-		__m128i p3bH = _mm_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 28)));
-		__m128i sumaL = _mm_adds_epu16(p3aL, _mm_adds_epu16(p1aL, p2aL));
-		__m128i sumaH = _mm_adds_epu16(p3aH, _mm_adds_epu16(p1aH, p2aH));
-		__m128i sumbL = _mm_adds_epu16(p3bL, _mm_adds_epu16(p1bL, p2bL));
-		__m128i sumbH = _mm_adds_epu16(p3bH, _mm_adds_epu16(p1bH, p2bH));
-		__m128i sclaL = _mm_mulhi_epu16(sumaL, _mm_set1_epi16(21846));
-		__m128i sclaH = _mm_mulhi_epu16(sumaH, _mm_set1_epi16(21846));
-		__m128i sclbL = _mm_mulhi_epu16(sumbL, _mm_set1_epi16(21846));
-		__m128i sclbH = _mm_mulhi_epu16(sumbH, _mm_set1_epi16(21846));
-		__m128i shftaL = _mm_shuffle_epi8(sclaL, _mm_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
-		__m128i shftaH = _mm_shuffle_epi8(sclaH, _mm_setr_epi8(-1, -1, -1, 18, 24, 30, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
-		__m128i shftbL = _mm_shuffle_epi8(sclbL, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, 0, 6, 12, -1, -1, -1, -1, -1, -1, -1));
-		__m128i shftbH = _mm_shuffle_epi8(sclbH, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1));
-		__m128i accumL = _mm_or_si128(shftaL, shftbL);
-		__m128i accumH = _mm_or_si128(shftaH, shftbH);
-		h3 = _mm_or_si128(accumL, accumH);
-#else
-		__m256i p1a = _mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt)));
-		__m256i p1b = _mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 18)));
-		__m256i p2a = _mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 1)));
-		__m256i p2b = _mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 19)));
-		__m256i p3a = _mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 2)));
-		__m256i p3b = _mm256_cvtepu8_epi16(_mm_loadu_si128(reinterpret_cast<const __m128i*>(pt + 20)));
-		__m256i suma = _mm256_adds_epu16(p3a, _mm256_adds_epu16(p1a, p2a));
-		__m256i sumb = _mm256_adds_epu16(p3b, _mm256_adds_epu16(p1b, p2b));
-		__m256i scla = _mm256_mulhi_epu16(suma, _mm256_set1_epi16(21846));
-		__m256i sclb = _mm256_mulhi_epu16(sumb, _mm256_set1_epi16(21846));
-		__m256i shfta = _mm256_shuffle_epi8(scla, _mm256_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
-		__m256i shftb = _mm256_shuffle_epi8(sclb, _mm256_setr_epi8(-1, -1, -1, -1, -1, -1, 0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 18, 24, 30, -1, -1, -1, -1));
-		__m256i accum = _mm256_or_si256(shfta, shftb);
-		h3 = _mm_or_si128(_mm256_castsi256_si128(accum), _mm256_extracti128_si256(accum, 1));
-#endif
+		__m256i in1 = _mm256_castsi128_si256(_mm_loadu_si128((const __m128i*)(pt)));
+		in1 = _mm256_inserti128_si256(in1, _mm_loadu_si128((const __m128i*)(pt + 15)), 1);
+		__m256i b1 = _mm256_shuffle_epi8(in1, _mm256_setr_epi8(0, -1, 3, -1, 6, -1, 9, -1, 12, -1, -1, -1, -1, -1, -1, -1, 0, -1, 3, -1, 6, -1, 9, -1, 12, -1, -1, -1, -1, -1, -1, -1));
+		__m256i g1 = _mm256_shuffle_epi8(in1, _mm256_setr_epi8(1, -1, 4, -1, 7, -1, 10, -1, 13, -1, -1, -1, -1, -1, -1, -1, 1, -1, 4, -1, 7, -1, 10, -1, 13, -1, -1, -1, -1, -1, -1, -1));
+		__m256i r1 = _mm256_shuffle_epi8(in1, _mm256_setr_epi8(2, -1, 5, -1, 8, -1, 11, -1, 14, -1, -1, -1, -1, -1, -1, -1, 2, -1, 5, -1, 8, -1, 11, -1, 14, -1, -1, -1, -1, -1, -1, -1));
+		__m256i sum = _mm256_adds_epu16(r1, _mm256_adds_epu16(b1, g1));
+		__m256i accum = _mm256_mulhrs_epi16(sum, _mm256_set1_epi16(10923));
+		__m256i shuf = _mm256_shuffle_epi8(accum, _mm256_setr_epi8(0, 2, 4, 6, 8, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 2, 4, 6, 8, -1, -1, -1, -1, -1, -1));
+		h3 = _mm_or_si128(_mm256_extracti128_si256(shuf, 1), _mm256_castsi256_si128(shuf));
 	}
 	if (last_row_and_col) {
 		switch (cols_minus_j) {
+		case 15:
+			out[14] = static_cast<uint8_t>(_mm_extract_epi8(h3, 14));
+		case 14:
+			out[13] = static_cast<uint8_t>(_mm_extract_epi8(h3, 13));
+		case 13:
+			out[12] = static_cast<uint8_t>(_mm_extract_epi8(h3, 12));
 		case 12:
 			out[11] = static_cast<uint8_t>(_mm_extract_epi8(h3, 11));
 		case 11:
@@ -184,19 +139,19 @@ void process(const uint8_t* __restrict const pt, const int32_t cols_minus_j, uin
 template<bool last_row, bool weight>
 void processRow(const uint8_t* __restrict pt, const int32_t cols, uint8_t* const __restrict out) {
 	int j = 0;
-	for (; j < cols - 12; j += 12, pt += 36) {
+	for (; j < cols - 10; j += 10, pt += 30) {
 		process<false, weight>(pt, cols - j, out + j);
 	}
 	process<last_row, weight>(pt, cols - j, out + j);
 }
 
 template<bool weight>
-void _RGB2Y(const uint8_t* __restrict const data, const int32_t cols, const int32_t start_row, const int32_t rows, const int32_t stride, uint8_t* const __restrict out) {
+void __forceinline _RGB2Y(const uint8_t* __restrict const data, const int32_t cols, const int32_t start_row, const int32_t rows, const int32_t stride, uint8_t* const __restrict out) {
 	int i = start_row;
 	for (; i < start_row + rows - 1; ++i) {
-		processRow<false, weight>(data + 3 * i * stride, cols, out + i*cols);
+		processRow<false, weight>(data + 3 * i * stride, cols, out + i * cols);
 	}
-	processRow<true, weight>(data + 3 * i * stride, cols, out + i*cols);
+	processRow<true, weight>(data + 3 * i * stride, cols, out + i * cols);
 }
 
 template<bool multithread, bool weight>
@@ -209,7 +164,7 @@ void RGB2Y(const uint8_t* const __restrict image, const int width, const int hei
 			int i = 0, start = 0;
 			for (; i < std::min(height - 1, hw_concur - 1); ++i, start += thread_stride) {
 				fut[i] = std::async(std::launch::async, _RGB2Y<weight>, image, width, start, thread_stride, stride, out);
-			}	
+			}
 			fut[i] = std::async(std::launch::async, _RGB2Y<weight>, image, width, start, height - start, stride, out);
 			for (int j = 0; j <= i; ++j) fut[j].wait();
 		}
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 *   RGB2Y
 *
 *	Author: Kareem Omar
-*	kareem.omar@uah.edu
+*	kareem.h.omar@gmail.com
 *	https://github.com/komrad36
 *
-*	Last updated Dec 9, 2016
+*	Last updated Sep 29, 2018
 *******************************************************************/
 //
 // Fastest CPU (AVX/SSE) implementation of RGB to grayscale.
@@ -17,32 +17,30 @@
 //
 // You can use equal weighting by calling the templated
 // function with weight set to 'false', or you
-// can specify custom weights in RGB2Y.h (only slightly slower).
+// can specify non-equal weights (only slightly slower).
 //
-// The default weights match OpenCV's default.
+// The default non-equal weights match OpenCV's default.
 //
 // For even more speed see the CUDA version:
 // github.com/komrad36/CUDARGB2Y
 //
-// If you do not have AVX2, uncomment the '#define NO_AVX_PLEASE' in RGB2Y.h to route the code
-// through SSE isntructions only. NOTE THAT THIS IS ABOUT 50% SLOWER.
+// If you do not have AVX2, uncomment the #define NO_AVX_PLEASE below to
+// use only SSE isntructions. NOTE THAT THIS IS ABOUT 50% SLOWER.
 // A processor with full AVX2 support is highly recommended.
-// 
+//
 // All functionality is contained in RGB2Y.h.
 // 'main.cpp' is a demo and test harness.
 //
+
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
 
 #include <chrono>
 #include <iomanip>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <string>
-#include <vector>
 
 #include "RGB2Y.h"
-
-#define VC_EXTRALEAN
-#define WIN32_LEAN_AND_MEAN
 
 using namespace std::chrono;
 
@@ -56,52 +54,74 @@ void printReport(const std::string& name, const nanoseconds& dur, const nanoseco
 	}
 }
 
-void RGB2Y_ref(const uint8_t* __restrict const data, const int32_t cols, const int32_t rows, const int32_t stride, uint8_t* const __restrict out) {
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < cols; ++j) {
-			const auto idx = 3 * (i*stride + j);
-			out[i*stride + j] = (static_cast<uint32_t>(data[idx]) + static_cast<uint32_t>(data[idx + 1]) + static_cast<uint32_t>(data[idx + 2])) / 3;
-		}
-	}
-}
-
-
 int main() {
 	// ------------- Configuration ------------
-	constexpr bool display_image = false;
-	constexpr auto warmups = 500;
-	constexpr auto runs = 2000;
+	constexpr auto warmups = 50;
+	constexpr auto runs = 100;
+
+	// OpenCV's impl is multithreaded so set to true for fair comparison
 	constexpr bool multithread = true;
-	constexpr bool weighted_averaging = false;
+
+	constexpr bool weighted_averaging = true;
+
 	constexpr char name[] = "test.jpg";
+	constexpr bool generate_test_pixels = true;
 	// --------------------------------
 
 
 	// ------------- Image Read ------------
-	cv::Mat image = cv::imread(name);
-	if (!image.data) {
-		std::cerr << "ERROR: failed to open image. Aborting." << std::endl;
-		return EXIT_FAILURE;
+	cv::Mat image;
+	if (!generate_test_pixels) {
+		image = cv::imread(name);
+		if (!image.data) {
+			std::cerr << "ERROR: failed to open image. Aborting." << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		constexpr int patch_size = 3 * 256 * 256 * 256;
+		image = cv::Mat(256, 256 * 256, CV_8UC3, new uint8_t[patch_size + 128]);
+		int x = 0;
+		for (int i = 0; i < 256; ++i) {
+			for (int j = 0; j < 256; ++j) {
+				for (int k = 0; k < 256; ++k) {
+					image.data[x++] = i;
+					image.data[x++] = j;
+					image.data[x++] = k;
+				}
+			}
+		}
 	}
 	// --------------------------------
 
-	
+
 	// ------------- Ref ------------
-	uint8_t* refresult = new uint8_t[image.cols*image.rows];
-	RGB2Y_ref(image.data, image.cols, image.rows, image.cols, refresult);
+	nanoseconds RGB2Y_ref_ns;
+	uint8_t* refimage = new uint8_t[image.cols*image.rows];
+	std::cout << std::endl << "------------- Ref ------------" << std::endl << "Warming up..." << std::endl;
+	for (int i = 0; i < 0; ++i) RGB2Y_ref<weighted_averaging>(image.data, image.cols, image.rows, image.cols, refimage);
+	std::cout << "Testing..." << std::endl;
+	{
+		const high_resolution_clock::time_point start = high_resolution_clock::now();
+		for (int32_t i = 0; i < 1; ++i) {
+			RGB2Y_ref<weighted_averaging>(image.data, image.cols, image.rows, image.cols, refimage);
+		}
+		const high_resolution_clock::time_point end = high_resolution_clock::now();
+		RGB2Y_ref_ns = end - start;
+	}
 	// --------------------------------
 
 
 	// ------------- RGB2Y ------------
 	nanoseconds RGB2Y_ns;
-	uint8_t* newimage = new uint8_t[image.cols*image.rows];
-	std::cout << std::endl << "------------- RGB2Y ------------" << std::endl << "Warming up..." << std::endl;
-	for (int i = 0; i < warmups; ++i) RGB2Y<multithread, weighted_averaging>(image.data, image.cols, image.rows, image.cols, newimage);
+	uint8_t* rgb2y_image = new uint8_t[image.cols*image.rows];
+	std::cout << "------------- RGB2Y ------------" << std::endl << "Warming up..." << std::endl;
+	for (int i = 0; i < warmups; ++i) RGB2Y<multithread, weighted_averaging>(image.data, image.cols, image.rows, image.cols, rgb2y_image);
 	std::cout << "Testing..." << std::endl;
 	{
 		const high_resolution_clock::time_point start = high_resolution_clock::now();
 		for (int32_t i = 0; i < runs; ++i) {
-			RGB2Y<multithread, weighted_averaging>(image.data, image.cols, image.rows, image.cols, newimage);
+			RGB2Y<multithread, weighted_averaging>(image.data, image.cols, image.rows, image.cols, rgb2y_image);
 		}
 		const high_resolution_clock::time_point end = high_resolution_clock::now();
 		RGB2Y_ns = (end - start) / runs;
@@ -112,16 +132,18 @@ int main() {
 	// ------------- OpenCV ------------
 	nanoseconds CV_ns;
 	cv::Mat newimage_cv;
-	std::cout << "------------ OpenCV ------------" << std::endl << "Warming up..." << std::endl;
-	for (int i = 0; i < warmups; ++i) cv::cvtColor(image, newimage_cv, CV_BGR2GRAY);
-	std::cout << "Testing..." << std::endl;
-	{
-		const high_resolution_clock::time_point start = high_resolution_clock::now();
-		for (int32_t i = 0; i < runs; ++i) {
-			cv::cvtColor(image, newimage_cv, CV_BGR2GRAY);
+	if (weighted_averaging) {
+		std::cout << "------------ OpenCV ------------" << std::endl << "Warming up..." << std::endl;
+		for (int i = 0; i < warmups; ++i) cv::cvtColor(image, newimage_cv, CV_BGR2GRAY);
+		std::cout << "Testing..." << std::endl;
+		{
+			const high_resolution_clock::time_point start = high_resolution_clock::now();
+			for (int32_t i = 0; i < runs; ++i) {
+				cv::cvtColor(image, newimage_cv, CV_BGR2GRAY);
+			}
+			const high_resolution_clock::time_point end = high_resolution_clock::now();
+			CV_ns = (end - start) / runs;
 		}
-		const high_resolution_clock::time_point end = high_resolution_clock::now();
-		CV_ns = (end - start) / runs;
 	}
 	// --------------------------------
 
@@ -129,29 +151,24 @@ int main() {
 	// ------------- Verification ------------
 	int i = 0;
 	size_t count = 0;
+	std::cout << std::endl;
 	for (; i < image.cols*image.rows; ++i) {
-		if (abs(newimage[i] - (weighted_averaging ? newimage_cv.data[i] : refresult[i])) > 1) {
+		if ((int)refimage[i] != (int)rgb2y_image[i]) {
 			//std::cerr << "ERROR! One or more pixels disagree!" << std::endl;
-			//std::cerr << i << ": got " << +newimage[i] << ", should be " << (weighted_averaging ? +newimage_cv.data[i] : +refresult[i]) << std::endl;
+			std::cerr << i << ": got " << +rgb2y_image[i] << ", should be " << /*(weighted_averaging ? */+refimage[i]/* : +refresult[i])*/ << std::endl;
 			++count;
 			//break;
 		}
 	}
 	//if (i == image.cols*image.rows) std::cout << "All pixels agree! Test valid." << std::endl << std::endl;
-	/*else */std::cout << count << " pixels disagree." << std::endl;
+	/*else */std::cout << count << " pixels disagree." << std::endl << std::endl;
 	// --------------------------------
 
 
 	// ------------- Output ------------
+	printReport("Ref", RGB2Y_ref_ns);
 	printReport("RGB2Y", RGB2Y_ns);
-	printReport("OpenCV", CV_ns, RGB2Y_ns);
+	if (weighted_averaging)	printReport("OpenCV", CV_ns, RGB2Y_ns);
 	std::cout << std::endl;
-	if (display_image) {
-		cv::namedWindow("RGB2Y", CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO);
-		cv::imshow("RGB2Y", cv::Mat(image.rows, image.cols, CV_8U, newimage));
-		cv::namedWindow("OpenCV", CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO);
-		cv::imshow("OpenCV", newimage_cv);
-		cv::waitKey(0);
-	}
 	// --------------------------------
 }
